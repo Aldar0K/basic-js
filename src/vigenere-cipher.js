6db94f0d789efba55c354ea5be0eddd2c5cd134f
Aldar <aldarkonov@mail.ru>
2022-05-01 15:34:45 +0300

    feat: 9 tasks done

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -19,14 +19,88 @@ const { NotImplementedError } = require('../extensions/index.js');
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
  * 
  */
+
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor (machineType = true) {
+    this.machineType = machineType;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if ((typeof message === 'undefined' || typeof key === 'undefined' || message === null || key === null)) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    message = message.toLowerCase();
+    key = key.toLowerCase();
+
+    let result = '';
+    let alphabet = 'abcdefghijklmnopqrstuvwxyz';
+    let messageLength = message.length;
+    let keyLength = key.length;
+
+    if (messageLength < keyLength) {
+      key = key.slice(0, messageLength);
+    }
+
+    if (messageLength > keyLength) {
+      let diffLength = Math.ceil(messageLength / keyLength);
+      key = key.repeat(diffLength).slice(0, messageLength);
+    }
+
+    let messageArr = message.split('');
+    let keyArr = key.split('');
+
+    messageArr.forEach(char => {
+      let indexOfCharInAlphabet = alphabet.indexOf(char);
+      if (indexOfCharInAlphabet < 0) {
+        result += char;
+      } else {
+        result += alphabet.charAt((indexOfCharInAlphabet + alphabet.indexOf(keyArr.shift())) % alphabet.length);
+      }
+    });
+
+    result = result.toUpperCase();
+
+    return (this.machineType) ? result : result.split('').reverse().join('');
+  }
+
+  decrypt(message, key) {
+    if ((typeof message === 'undefined' || typeof key === 'undefined' || message === null || key === null)) {
+      throw new Error('Incorrect arguments!')
+    }
+
+    message = message.toLowerCase();
+    key = key.toLowerCase();
+
+    let result = '';
+    let alphabet = 'abcdefghijklmnopqrstuvwxyz';
+    let messageLength = message.length;
+    let keyLength = key.length;
+
+    if (messageLength < keyLength) {
+      key = key.slice(0, messageLength);
+    }
+
+    if (messageLength > keyLength) {
+      let diffLength = Math.ceil(messageLength / keyLength);
+      key = key.repeat(diffLength).slice(0, messageLength);
+    }
+
+    let messageArr = message.split('');
+    let keyArr = key.split('');
+
+    messageArr.forEach(char => {
+      let indexOfCharInAlphabet = alphabet.indexOf(char);
+      if (indexOfCharInAlphabet < 0) {
+        result += char;
+      } else {
+        result += alphabet.charAt((indexOfCharInAlphabet + alphabet.length - alphabet.indexOf(keyArr.shift())) % alphabet.length);
+      }
+    });
+
+    result = result.toUpperCase();
+
+    return (this.machineType) ? result : result.split('').reverse().join('');
   }
 }
 
